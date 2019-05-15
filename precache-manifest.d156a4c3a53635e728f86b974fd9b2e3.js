a223d9364fa671c097bff09e9f3e140b20df38dc
Dejan <dejan.tomic@yahoo.com>
2019-05-15 22:52:15 +0200

    e86e21f

---
+++ precache-manifest.d156a4c3a53635e728f86b974fd9b2e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/map-app/robots.txt"
+  },
+  {
+    "revision": "71eea65f5d071d73eec3",
+    "url": "/map-app/js/chunk-vendors.a45ecb48.js"
+  },
+  {
+    "revision": "5d66fedd6bf94d6357e5",
+    "url": "/map-app/js/app.8459accd.js"
+  },
+  {
+    "revision": "982dee32a74c6138a04c",
+    "url": "/map-app/js/about.a0927122.js"
+  },
+  {
+    "revision": "36e4b4705915cbc98172b77f082bbe89",
+    "url": "/map-app/index.html"
+  },
+  {
+    "revision": "71eea65f5d071d73eec3",
+    "url": "/map-app/css/chunk-vendors.02d743b0.css"
+  },
+  {
+    "revision": "5d66fedd6bf94d6357e5",
+    "url": "/map-app/css/app.48a8d697.css"
+  }
+];
